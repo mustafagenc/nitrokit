ecbf61ba0d10af38b4fd0878aecd44ee8f201a0d
Mustafa Genc <eposta@mustafagenc.info>
2025-06-12 20:09:34 +0300

    Renames brand icons to kebab case
    Updates brand icon filenames to use kebab case for consistency.
    
    Updates the brand icon index file to reflect the filename changes.
    
    Removes flag icons.

--- src/components/images/flags/De.tsx
+++
@@ -1,9 +0,0 @@
-import { SVGProps } from 'react';
-
-export const De = (props: SVGProps<SVGSVGElement>) => (
-    <svg xmlns="http://www.w3.org/2000/svg" id="flag-icons-de" viewBox="0 0 640 480" {...props}>
-        <path fill="#fc0" d="M0 320h640v160H0z" />
-        <path fill="#000001" d="M0 0h640v160H0z" />
-        <path fill="red" d="M0 160h640v160H0z" />
-    </svg>
-);
